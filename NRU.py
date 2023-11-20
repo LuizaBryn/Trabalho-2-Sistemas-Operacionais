09bbcad95e6cd51286ec7b6a07045b26fc174cd6
Luiza Bryn <98985775+LuizaBryn@users.noreply.github.com>
2023-11-20 15:34:02 -0300

    feat: contagem de tempo de execução (NRU e Relogio)

--- NRU.py
+++ NRU.py
@@ -1,9 +1,39 @@
+import time
 
-"CÓDIGO"
+def nru(num_quadros, lista_referencias):
+    
+    tempo_inicial = time.time()
 
-...
+    page_faults = 0
+
+    ...
+
+    tempo_final = time.time()
+    tempo_execucao = tempo_final - tempo_inicial
+    tempo_execucao = round(tempo_execucao, 4)
+
+    print(f"Tempo de execução: {tempo_execucao} segundos")
+
+    return page_faults
+
+#Leitura arquivo de testes 1
+with open('input_teste1.txt', 'r') as file:
+    linhas = file.readlines()
+
+lista_referencias = [linha.strip() for linha in linhas[1:]]
+num_quadros = int(linhas[0].strip()) 
+
+#TESTE 1
+page_faults = nru(num_quadros, lista_referencias)
+print(f"Houve {page_faults} page faults!")
+
+#Leitura arquivo de testes 2
 with open('input_teste2.txt', 'r') as file:
     linhas = file.readlines()
 
 lista_referencias = [linha.strip() for linha in linhas[1:]]
 num_quadros = int(linhas[0].strip()) 
+
+#TESTE 2
+page_faults = nru(num_quadros, lista_referencias)
+print(f"Houve {page_faults} page faults!")
--- Relogio_e_segunda_chance.py
+++ Relogio_e_segunda_chance.py
@@ -1,8 +1,18 @@
 import time
 
-"CÓDIGO"
+def relogio(num_quadros, lista_referencias):
+    tempo_inicial = time.time()
 
-...
+    page_faults = 0
+    ...
+    tempo_final = time.time()
+    tempo_execucao = tempo_final - tempo_inicial
+    tempo_execucao = round(tempo_execucao, 4)
+
+    print(f"Tempo de execução: {tempo_execucao} segundos")
+
+    return page_faults
+
 #Leitura arquivo de testes 1
 with open('input_teste1.txt', 'r') as file:
     linhas = file.readlines()
@@ -11,7 +21,8 @@ lista_referencias = [linha.strip() for linha in linhas[1:]]
 num_quadros = int(linhas[0].strip()) 
 
 #TESTE 1
-...
+page_faults = relogio(num_quadros, lista_referencias)
+print(f"Houve {page_faults} page faults!")
 
 #Leitura arquivo de testes 2
 with open('input_teste2.txt', 'r') as file:
@@ -21,4 +32,5 @@ lista_referencias = [linha.strip() for linha in linhas[1:]]
 num_quadros = int(linhas[0].strip()) 
 
 #TESTE 2
-...
+page_faults = relogio(num_quadros, lista_referencias)
+print(f"Houve {page_faults} page faults!")
